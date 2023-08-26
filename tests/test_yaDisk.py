a2f317cd5ca7274ec72675f68c754ae40d66d2dd
lomik31 <lomik31@codead.dev>
2023-08-26 16:03:39 +0300

    moved creating disk class instance outside test functions. also renamed test folder

--- tests/test_yaDisk.py
+++ tests/test_yaDisk.py
@@ -4,15 +4,15 @@ from disk import Disk
 class TestDisk(unittest.TestCase):
     with open("./tests/yaDisk") as f:
         config = f.read()
+    d = Disk(config)
 
     def testSuccess(self):
-        d = Disk(self.config)
-        fName = "amogus"
-        res = d.createFolder(fName)
+        fName = "test1"
+        res = self.d.createFolder(fName)
         self.assertEqual(res.status_code, 201)
 
         params = {
             "path": fName
         }
-        res_test = requests.get("https://cloud-api.yandex.net/v1/disk/resources", headers=d.headers, params=params)
+        res_test = requests.get("https://cloud-api.yandex.net/v1/disk/resources", headers=self.d.headers, params=params)
         self.assertEqual(res_test.status_code, 200)
