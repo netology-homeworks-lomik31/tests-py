0600c58b4a09eceadc92f1fcd428f361a8fc8614
lomik31 <lomik31@codead.dev>
2023-08-26 16:11:03 +0300

    added duplicate test function

--- tests/test_yaDisk.py
+++ tests/test_yaDisk.py
@@ -16,3 +16,17 @@ class TestDisk(unittest.TestCase):
         }
         res_test = requests.get("https://cloud-api.yandex.net/v1/disk/resources", headers=self.d.headers, params=params)
         self.assertEqual(res_test.status_code, 200)
+    
+    def testDuplicate(self):
+        fName = "test2"
+        
+        for _ in range(2):
+            res = self.d.createFolder(fName)
+        
+        self.assertEqual(res.status_code, 409)
+
+        params = {
+            "path": fName
+        }
+        res_test = requests.get("https://cloud-api.yandex.net/v1/disk/resources", headers=self.d.headers, params=params)
+        self.assertEqual(res_test.status_code, 200)
