24df2ca52a074d1efcdde20271e072055e42c702
lomik31 <lomik31@codead.dev>
2023-08-26 15:55:19 +0300

    added success test for yaDisk

--- tests/test_yaDisk.py
+++ tests/test_yaDisk.py
@@ -2,4 +2,16 @@ import requests, unittest
 from disk import Disk
 
 class TestDisk(unittest.TestCase):
-    pass
+    def testSuccess(self):
+        with open("./tests/yaDisk") as f:
+            config = f.read()
+        d = Disk(config)
+        fName = "amogus"
+        res = d.createFolder(fName)
+        self.assertEqual(res.status_code, 201)
+
+        params = {
+            "path": fName
+        }
+        res_test = requests.get("https://cloud-api.yandex.net/v1/disk/resources", headers=d.headers, params=params)
+        self.assertEqual(res_test.status_code, 200)
